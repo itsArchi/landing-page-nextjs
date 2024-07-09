b42138513841fad5e1816dd0ecdd460ad9f8e0f7
rohim48 <noerrohim10@gmail.com>
2024-07-09 21:11:58 +0700

    added image

---
+++ public/index.js
@@ -0,0 +1,24 @@
+import LogoImg from './logo.png';
+import HeroImg from './rentcar.png';
+import LogoToyota from './toyota.jpg';
+import LogoDaihatsu from './daihatsu.png';
+import LogoMazda from './mazda.png';
+import LogoHyundai from './hyundai.jpg';
+import LogoMitsubishi from './mitsubishi.webp';
+import LogoSuzuki from './suzuki.jpg';
+import CarAvanza from './avanza.png';
+import CarCalya from './calya.png';
+import CarCreta from './creta.png';
+import CarCx from './cx5.png';
+import CarErtiga from './ertiga.png';
+import CarSigra from './sigra.png';
+import CarStargazer from './stargazer.png';
+import CarXenia from './xenia.png';
+import CarXl from './xl7.png';
+import CarXpander from './xpander.png';
+import CarZenix from './zenix.png';
+
+export { 
+    LogoImg, HeroImg, LogoToyota, LogoDaihatsu, LogoMazda, LogoHyundai, LogoMitsubishi, LogoSuzuki, 
+    CarAvanza, CarCalya, CarCreta, CarCx, CarErtiga, CarSigra, CarStargazer, CarXenia, CarXl, CarXpander, CarZenix
+}
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Hero from "./Hero";
+import List from "./List";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export { Hero, List, Testimonials, Footer, Navbar };
