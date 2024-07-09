d0af0ab90bb73c8e2cc52d7644f84042e6975c33
rohim48 <noerrohim10@gmail.com>
2024-07-09 21:51:39 +0700

    added navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import LogoImg from "../../../public/logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <section>
+      <div>
+        <nav className="pt-10 pr-10 pl-10 pb-5 mb-12 flex justify-between">
+          <Image src={LogoImg} />
+          <div>
+            <form></form>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+};
 
-export default Navbar
+export default Navbar;
